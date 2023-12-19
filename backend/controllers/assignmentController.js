ddbbcb9c3d6e597c48d557dcf490187de2186111
unknown <ayanahye2@gmail.com>
2023-12-19 00:45:47 -0800

    added REST API for assignment is done

--- backend/controllers/assignmentController.js
+++ backend/controllers/assignmentController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler')
 
+const Assignment = require('../models/assignmentModel')
 // get the assignments
 // GET api/assignments
 // private
 const getAssignments = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get assignments'})
+    const assignments = await Assignment.find()
+    res.status(200).json(assignments)
 })
 
 // set the assignment
@@ -16,21 +18,43 @@ const setAssignment = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Add assignment'})
+
+    const assignment = await Assignment.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(assignment)
 })
 
 // update the assignment
 // PUT api/assignments/:id
 // private
 const updateAssignment = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update assignment ${req.params.id}`})
+    const assignment = await Assignment.findById(req.params.id)
+
+    if (!assignment) {
+        res.status(400)
+        throw new Error('Assignment not found');
+    }
+
+    const updatedAssignment = await Assignment.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    res.status(200).json(updateAssignment);
 })
 
 // delete the assignment
 // DELETE api/assignments/:id
 // private
 const deleteAssignment = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete assignment ${req.params.id}`})
+    const assignment = await Assignment.findById(req.params.id)
+
+    if (!assignment) {
+        res.status(400)
+        throw new Error('Assignment not found');
+    }
+
+    await assignment.deleteOne();
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
